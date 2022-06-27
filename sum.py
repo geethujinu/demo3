25cd6145e91d68c53c6f8096d880a11788ac7046
geethujinu <107743250+geethujinu@users.noreply.github.com>
2022-06-27 13:16:13 +0530

    some editing done

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
-def add_num(a,b):
+def add_numbers(a,b):
     sum=a+b;
     return sum;
 num1=int(input("input the number one: "))
-num2=int(input("input the number one :"))
-print("The sum is",add_num(num1,num2))
+num2=int(input("input the number two :"))
+print("The sum is",add_numbers(num1,num2))
